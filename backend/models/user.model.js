8854be0955c71d4f15260aac0615fe92600b24b0
ojasss11 <bidkarojas@gmail.com>
2024-09-30 16:27:07 +0530

    separated front-end and backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false, // you can remove this if you are disabling verification
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
